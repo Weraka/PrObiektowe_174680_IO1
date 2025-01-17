7fc730aae24d614d4ef60d132901a0f4e54c3fd1
Weraka <147046147+Weraka@users.noreply.github.com>
2025-01-17 15:11:25 +0100

    Zadanie z 17.01

---
+++ 17.01/src/Main.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class Main {
+
+    //A1
+    public static <T> void printUnique(Collection<T> items)
+    {
+        Set<T> uniqueElements = new HashSet<T>(items);
+        for(T item : uniqueElements)
+        {
+            System.out.println(item);
+        }
+    }
+
+    //A2
+    public static <T> ArrayList<T> mergeLists(ArrayList<T> list1, ArrayList<T> list2)
+    {
+        ArrayList<T> merged = new ArrayList<>();
+
+        merged.addAll(list1);
+        merged.addAll(list2);
+
+        return merged;
+    }
+
+    //A3
+    public static <T> void removeEveryOther(Collection<T> items)
+    {
+        items.removeIf(item -> (int) item % 2 == 0);
+        for(T item : items)
+        {
+            System.out.println(item);
+        }
+    }
+
+    //B1
+    public static <T> void reversePrint(Iterable<T> items)
+    {
+        
+
+    }
+
+    //F1
+    public static <T extends Comparable<T>> TreeSet<T> findElementsInRange(TreeSet<T> set, T lowerBound, T upperBound)
+    {
+        TreeSet<T> treeSet = new TreeSet<>(set.subSet(lowerBound, true, upperBound, true));
+
+        return treeSet;
+    }
+
+    public static void main(String[] args)
+    {
+        /*int lowerBound = 9;
+        int upperBound = 20;
+        TreeSet<Integer> treeSet  = new TreeSet<>();
+        treeSet.add(20);
+        treeSet.add(12);
+        treeSet.add(3);
+        treeSet.add(9);
+        treeSet.add(21);
+
+        System.out.println(findElementsInRange(treeSet,lowerBound,upperBound));*/
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(1);
+        list.add(2);
+
+        removeEveryOther(list);
+
+        /*ArrayList<Integer> list1 = new ArrayList<>();
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+
+        System.out.println(mergeLists(list, list1));*/
+
+
+
+
+    }
+}
